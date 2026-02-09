74e27d2ac2b43d539b161545029fd82184f68f0d
Aung <august810.a@gmail.com>
2026-02-09 14:39:15 +0700

    fixed image

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -50,7 +50,7 @@ export default function EventDetailPage() {
             alt={event.title}
             fill
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            className="object-center transition-transform duration-500 group-hover:scale-105"
+            className=" object-cover transition-transform duration-500 group-hover:scale-105"
           />
         </div>
 
